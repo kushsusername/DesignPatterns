1010231d17282d03cd87bf73bbb552cbbad7413b
Kush <patel.kush.d@gmail.com>
2015-10-06 02:49:26 -0400

    putting in the design document

--- designpatterns/src/cscie97/asn2/housemate/test/TestDriver.java
+++ designpatterns/src/cscie97/asn2/housemate/test/TestDriver.java
@@ -2,4 +2,10 @@ package cscie97.asn2.housemate.test;
 
 public class TestDriver {
 
+	public void main(String[] args) {
+		if(args[0] == null){
+			System.out.println("Please provide an input setup file for the housemate system.");
+			return;
+		}
+	}
 }
